20e43814fa0fc892cf011dbf7d74cb7811c0c396
Narendra Nanotkar <47916861+narendra1416@users.noreply.github.com>
2020-04-12 17:19:40 +0530

    key

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Lockdown is not over');
+  res.send('Lockdown Key');
 });
 
 app.listen(PORT, HOST);
